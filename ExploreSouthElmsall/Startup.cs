28b7a4e37a4a6bd772fa4203f256c9d88a991bd2
JoshuaDouce <Joshuadouce@gmail.com>
2019-06-01 21:42:37 +0100

    .net core web app using only static files

--- ExploreSouthElmsall/Startup.cs
+++ ExploreSouthElmsall/Startup.cs
@@ -5,30 +5,40 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ExploreSouthElmsall
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient(x => 
+            new FeatureToggles {
+                EnableDeveloperExceptions = _configuration.GetValue<bool>("FeatureToggles:EnableDeveloperExceptions") });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, FeatureToggles features)
         {
-            if (env.IsDevelopment())
+            app.UseExceptionHandler("/error.html");
+
+            if (features.EnableDeveloperExceptions)
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseFileServer();
         }
     }
 }
